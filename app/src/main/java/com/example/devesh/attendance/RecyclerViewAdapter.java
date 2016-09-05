d9e565c4bb7ecb04d737e49eafb410d32d437432
Devesh Saini <deveshsaini96@gmail.com>
2016-09-05 13:46:05 +0530

    Expandable Java Fix

--- app/src/main/java/com/example/devesh/attendance/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/devesh/attendance/RecyclerViewAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -16,19 +17,23 @@ import java.util.HashSet;
 
     public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ReyclerViewHolder> {
 
-        private LayoutInflater layoutInflater;
+        ArrayList<Info> mList;
+
+
+    private LayoutInflater layoutInflater;
         private HashSet<Integer> expandedPositionSet;
         private Context context;
 
-        public RecyclerViewAdapter(Context context) {
+        public RecyclerViewAdapter(Context context,ArrayList<Info> List) {
             this.layoutInflater = LayoutInflater.from(context);
             expandedPositionSet = new HashSet<>();
             this.context = context;
+            mList = List;
         }
 
         @Override
         public ReyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View item = layoutInflater.inflate(viewType == 0 ? R.layout.item_odd : R.layout.item_even, parent, false);
+            View item = layoutInflater.inflate(R.layout.list_item , parent, false);
 
             return new ReyclerViewHolder(item);
         }
@@ -40,32 +45,39 @@ import java.util.HashSet;
 
         @Override
         public int getItemViewType(int position) {
-            return position % 2;
+            return 0;
         }
 
         @Override
         public int getItemCount() {
-            return 20;
+            return mList.size() ;
         }
 
         class ReyclerViewHolder extends RecyclerView.ViewHolder {
-            private ExpandableLayout expandableLayout;
+            private ExpandableView ExpandableView;
             private TextView showInfo;
+            private TextView code,teacher,past,percent,name;
+
 
             private ReyclerViewHolder(final View view) {
                 super(view);
-                expandableLayout = (ExpandableLayout) view.findViewById(R.id.expandable_layout);
+                ExpandableView = (ExpandableView) view.findViewById(R.id.expandable_layout);
                 showInfo = (TextView) view.findViewById(R.id.show_info);
+                code = (TextView) view.findViewById(R.id.sub_code);
+                teacher = (TextView) view.findViewById(R.id.sub_teacher);
+                past = (TextView) view.findViewById(R.id.sub_att);
+                name = (TextView) view.findViewById(R.id.sub_name);
+                percent = (TextView) view.findViewById(R.id.sub_per);
             }
 
             private void updateItem(final int position) {
-                expandableLayout.setOnExpandListener(new ExpandableLayout.OnExpandListener() {
+                ExpandableView.setOnExpandListener(new ExpandableView.OnExpandListener() {
                     @Override
                     public void onExpand(boolean expanded) {
                         registerExpand(position, showInfo);
                     }
                 });
-                expandableLayout.setExpand(expandedPositionSet.contains(position));
+                ExpandableView.setExpand(expandedPositionSet.contains(position));
 
             }
         }
@@ -73,12 +85,12 @@ import java.util.HashSet;
         private void registerExpand(int position, TextView textView) {
             if (expandedPositionSet.contains(position)) {
                 removeExpand(position);
-                textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.arrow_down, 0);
+
                 textView.setText("Show description");
                 Toast.makeText(context, "Position: " + position + " collapsed!", Toast.LENGTH_SHORT).show();
             } else {
                 addExpand(position);
-                textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.arrow_up, 0);
+
                 textView.setText("Hide description");
                 Toast.makeText(context, "Position: " + position + " expanded!", Toast.LENGTH_SHORT).show();
             }
